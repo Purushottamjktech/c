0facf03e2b89bad3c58a59818dd65f34f913f968
Purushottamjktech <98081521+Purushottamjktech@users.noreply.github.com>
2022-01-22 16:15:45 +0530

    Add files via upload

---
+++ swapping(ca by reference).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a=10;
+int b =20;
+printf("value before swapping a= %d,b=%d\n\n",a,b);
+swap(&a,&b);
+
+}
+
+void swap(int *a,int *b )
+{
+int temp;
+temp =*a;
+*a= *b;
+*b= temp;
+
+printf("after swapping a =%d, b= %d",*a,*b);
+}
