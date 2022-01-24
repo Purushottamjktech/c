a98907f32cfec1de3a0976c7cd9e1a6607b5034c
Purushottamjktech <98081521+Purushottamjktech@users.noreply.github.com>
2022-01-24 16:57:19 +0530

    updated code

--- copy one charecter to another.c
+++ copy one charecter to another.c
@@ -2,10 +2,21 @@
 #include<string.h>
 void main()
 {
-    char str[50], copy[50];
-    printf("Enter the string: ");
-    gets(str);
-    printf("\nString 1 = %s", str);
-    strcpy(copy, str);
-    printf("\nString 2 = %s", copy);
+    char original[100], dupicate[100];
+    printf("Enter original string\n");
+    gets(original);
+    copy_string(dupicate, original);
+    printf("Target string is \"%s\"\n", dupicate);
+    return 0;
 }
+
+void copy_string(char *dupicate, char *original)
+{
+    while(*original !=0 )
+    {
+        *dupicate = *original;
+        original++;
+        dupicate++;
+    }
+}
+
